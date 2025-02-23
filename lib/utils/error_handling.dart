bbeffd4c021b738bb1b4eb2142b24f6add9fe06f
Myo Thant <myothant.thedev@gmail.com>
2025-02-24 01:10:22 +0700

    chore(utils): add error handling class

---
+++ lib/utils/error_handling.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:quillhub/utils/constants.dart';
+
+class ErrorHandling {
+  static String handleHttpError(http.Response response) {
+    switch(response.statusCode){
+      case 422:
+        final errors = jsonDecode(response.body)['errors'];
+        return errors[errors.keys.elementAt(0)][0];
+      case 403:
+        return jsonDecode(response.body)['errors'];
+      default:
+        return Constants.internalError;
+    }
+  }
+}
