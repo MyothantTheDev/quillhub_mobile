481e8376cd21fb71accb10847d344871804ab1da
Myo Thant <myothant.thedev@gmail.com>
2025-02-24 01:22:53 +0700

    feat(services): add article services

---
+++ lib/services/article_service.dart
@@ -0,0 +1,39 @@
+import 'package:quillhub/models/article.dart';
+import 'package:quillhub/services/api_service.dart';
+import 'package:quillhub/utils/constants.dart';
+
+class ArticleService {
+  late ApiService _apiService;
+
+  ArticleService() {
+    _apiService = ApiService();
+  }
+
+  Future<List<Article>> getAllArticle() async {
+    List<Article> allArticles = [];
+    final response = await _apiService.getRequest(
+      Constants.postURL,
+      {'Accept': 'application/json'}
+    );
+    if (response.data != null && response.data is List<dynamic>) {
+      for(final item in response.data as List<dynamic>) {
+        allArticles.add(Article.fromJson(item as Map<String, dynamic>));
+      }
+      return allArticles;
+    }
+    allArticles.add(Article.errorCounter(response));
+    return allArticles;
+  }
+
+  Future<Article> getDetailArticle(int id) async {
+    final response = await _apiService.getRequest(
+      '${Constants.postURL}/$id',
+      {'Accept': 'application/json'}
+    );
+    if (response.error != null) {
+      return Article.errorCounter(response);
+    }
+    return Article.fromJson(response.data as Map<String, dynamic>);
+  }
+
+}
