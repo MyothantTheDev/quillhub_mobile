2e584614e9287973065ccd9fed125a12d3a69181
Myo Thant <myothant.thedev@gmail.com>
2025-02-24 01:13:19 +0700

    feat(widgets): add custom app bar widget

---
+++ lib/widgets/custom_nav_app_bar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quillhub/widgets/categories_bar.dart';
+import 'package:quillhub/widgets/profile_button_widget.dart';
+import 'package:quillhub/widgets/theme_mode_toggler.dart';
+
+class CustomNavAppBar extends StatelessWidget {
+
+  const CustomNavAppBar({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    // TODO: implement build
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: _CustomAppBar(),
+      ),
+      body: child,
+    );
+  }
+
+}
+
+class _CustomAppBar extends ConsumerWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return Container(
+        padding: const EdgeInsets.only(top: 55, left: 10, right: 10),
+        color: Color(0xff100d2b),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'QuillHub',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white70,
+                    fontSize: 25,
+                  ),
+                ),
+                Row(
+                  children: [
+                    ThemeModeToggle(),
+                    ProfileButtonWidget()
+                  ],
+                )
+              ],
+            ),
+            CategoriesBar()
+          ],
+        )
+    );
+  }
+
+}
