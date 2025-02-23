a44725a0a80665ffc9cb1d4b0cef31aeba4fecbf
Myo Thant <myothant.thedev@gmail.com>
2025-02-24 01:05:05 +0700

    feat(models): add User Model

---
+++ lib/models/user.dart
@@ -0,0 +1,39 @@
+import 'package:quillhub/models/base.dart';
+import 'package:quillhub/services/api_response.dart';
+class User extends Model {
+  int? id;
+  String? name;
+  String? image;
+  String? email;
+  String? token;
+
+
+  User({
+    this.id,
+    this.name,
+    this.image,
+    this.email,
+    this.token,
+    this.error,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['user']['id'],
+      name: json['user']['name'],
+      image: json['user']['image'],
+      email: json['user']['email'],
+      token: json['user']['token'],
+    );
+  }
+
+  factory User.errorCounter(ApiResponse response) {
+    return User(
+      error: response.error
+    );
+  }
+
+  @override
+  String? error;
+
+}
