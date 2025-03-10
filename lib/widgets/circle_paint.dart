7845e8caafe139b63ac839cbac573f9a2d4a3f84
Myo Thant <myothant.thedev@gmail.com>
2025-03-10 22:14:18 +0700

    feat(widgets): add circle painter widget to use in login screen background

---
+++ lib/widgets/circle_paint.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CirclePainter extends CustomPainter {
+  final Color circleColor;
+  CirclePainter({required this.circleColor});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+    ..color = circleColor;
+    final center = Offset(size.width / 2, size.height / 2);
+    canvas.drawCircle(center, size.width / 2, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+}
