2e6881df3c0ed3cdcd68918821b59d761b86d4a9
Myo Thant <myothant.thedev@gmail.com>
2025-03-13 23:53:13 +0700

    feat(widget): add clickable text widget

---
+++ lib/widgets/clickable_text.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Clickable_Text extends StatelessWidget {
+  final String linkName;
+  final Widget child;
+  const Clickable_Text({super.key, required this.linkName, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GestureDetector(
+      onTap: () {
+        // context.goNamed(linkName);
+        print(linkName);
+      },
+      child: child,
+    );
+  }
+
+}
