fb7023faa1bd6a855aff81a3bceaa2c24c21a28d
Myo Thant <myothant.thedev@gmail.com>
2025-03-14 20:33:11 +0700

    feat(widget): add register background initial

---
+++ lib/widgets/register_background.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:quillhub/widgets/background_circle.dart';
+
+class RegisterBackground extends StatelessWidget {
+  const RegisterBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.sizeOf(context).width;
+    final double height = MediaQuery.sizeOf(context).height;
+
+    return Stack(
+      children: [
+        Positioned(left: width * 0.35, top: -130,child: BackgroundCircles(size: width * 0.55, color: Colors.indigo.shade100)),
+        Positioned(left: width * 0.25, top: -150,child: BackgroundCircles(size: width * 0.55, color: Colors.indigo)),
+      ],
+    );
+  }
+  
+}
