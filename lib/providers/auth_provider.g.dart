c89211f73bb23999737614f7319b0b189e0d3a63
Myo Thant <myothant.thedev@gmail.com>
2025-03-31 01:03:39 +0700

    fix: Late initialization error concerned with auth provider and auth token service

--- lib/providers/auth_provider.g.dart
+++ lib/providers/auth_provider.g.dart
@@ -6,12 +6,12 @@ part of 'auth_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$authNotifierHash() => r'797de8fbbc28208055f9f0fb0edc2994ac5e6954';
+String _$authNotifierHash() => r'ff15067e74ebfc51d1e15bdc915043e68ed1908d';
 
 /// See also [AuthNotifier].
 @ProviderFor(AuthNotifier)
 final authNotifierProvider =
-    AutoDisposeAsyncNotifierProvider<AuthNotifier, AuthState>.internal(
+    AutoDisposeNotifierProvider<AuthNotifier, AuthState>.internal(
   AuthNotifier.new,
   name: r'authNotifierProvider',
   debugGetCreateSourceHash:
@@ -20,6 +20,6 @@ final authNotifierProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$AuthNotifier = AutoDisposeAsyncNotifier<AuthState>;
+typedef _$AuthNotifier = AutoDisposeNotifier<AuthState>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/services/auth/token_service.dart
+++ lib/services/auth/token_service.dart
@@ -2,33 +2,30 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenService {
   static const String _tokenKey = 'auth_token';
-  late SharedPreferences _prefs;
-
-  TokenService() {
-    _initializePreferences();
-  }
-
-  Future<void> _initializePreferences() async {
-    _prefs = await SharedPreferences.getInstance();
-  }
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   Future<String> getToken({String? tokenKey}) async {
-    return _prefs.getString(tokenKey ?? _tokenKey) ?? '';
+    final prefs = await _prefs;
+    return prefs.getString(tokenKey ?? _tokenKey) ?? '';
   }
 
   Future<int> getUerId() async {
-    return _prefs.getInt('userId') ?? 0;
+    final prefs = await _prefs;
+    return prefs.getInt('userId') ?? 0;
   }
 
   Future<void> saveToken(String value, {String? tokenKey}) async {
-    await _prefs.setString(tokenKey ?? _tokenKey, value);
+    final prefs = await _prefs;
+    await prefs.setString(tokenKey ?? _tokenKey, value);
   }
 
   Future<void> removeToken({String? tokenKey}) async {
-    await _prefs.remove(tokenKey ?? _tokenKey);
+    final prefs = await _prefs;
+    await prefs.remove(tokenKey ?? _tokenKey);
   }
 
   Future<void> clearToken() async {
-    await _prefs.clear();
+    final prefs = await _prefs;
+    await prefs.clear();
   }
 }
