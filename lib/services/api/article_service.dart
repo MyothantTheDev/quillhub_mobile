c7325a8935b130a928f724835033c604956838de
Myo Thant <myothant.thedev@gmail.com>
2025-04-05 23:08:21 +0700

    feat(services): add specific error handling methods

--- lib/services/api/article_service.dart
+++ lib/services/api/article_service.dart
@@ -1,3 +1,4 @@
+import 'package:quillhub/models/api_response.dart';
 import 'package:quillhub/models/article.dart';
 import 'package:quillhub/services/api/api_service.dart';
 import 'package:quillhub/utils/core/constants.dart';
@@ -9,31 +10,46 @@ class ArticleService {
     _apiService = ApiService();
   }
 
-  Future<List<Article>> getAllArticle() async {
+  Future<List<Article>> getAllArticle(int page) async {
     List<Article> allArticles = [];
-    final response = await _apiService.getRequest(
-      Constants.postURL,
-      headers: {'Accept': 'application/json'}
-    );
-    if (response.data != null && response.data is List<dynamic>) {
-      for(final item in response.data as List<dynamic>) {
-        allArticles.add(Article.fromJson(item as Map<String, dynamic>));
+    try {
+
+      final response = await _apiService.getRequest(
+          '${Constants.postURL}/$page',
+          headers: {'Accept': 'application/json'}
+      );
+
+      if (response.data != null && response.data is List<dynamic>) {
+        for(final item in response.data as List<dynamic>) {
+          try {
+            allArticles.add(Article.fromJson(item as Map<String, dynamic>));
+          } catch (e) {
+            print('Error parsing article: $e');
+          }
+        }
       }
-      return allArticles;
+      allArticles.add(Article.errorCounter(response));
+    } catch (e) {
+      print('Error fetching article: $e');
+      allArticles.add(Article.errorCounter(ApiResponse()..message = e.toString()));
     }
-    allArticles.add(Article.errorCounter(response));
     return allArticles;
   }
 
-  Future<Article> getDetailArticle(int id) async {
-    final response = await _apiService.getRequest(
-      '${Constants.postURL}/$id',
-      headers: {'Accept': 'application/json'}
-    );
-    if (response.error != null) {
-      return Article.errorCounter(response);
+  Future<Article> getDetailArticle(String id) async {
+    try {
+      final response = await _apiService.getRequest(
+          '${Constants.postURL}/$id',
+          headers: {'Accept': 'application/json'}
+      );
+      if (response.status != 200) {
+        return Article.errorCounter(response);
+      }
+      return Article.fromJson(response.data as Map<String, dynamic>);
+    } catch (e) {
+      print('Error fetching article: $e');
+      return Article.errorCounter(ApiResponse()..message = e.toString());
     }
-    return Article.fromJson(response.data as Map<String, dynamic>);
   }
 
 }
