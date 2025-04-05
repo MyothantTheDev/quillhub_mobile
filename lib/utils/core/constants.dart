f7f9e203a16e656bff00e037e712b4b811474a79
Myo Thant <myothant.thedev@gmail.com>
2025-04-05 23:08:53 +0700

    feat(services): add post detail url

--- lib/utils/core/constants.dart
+++ lib/utils/core/constants.dart
@@ -2,11 +2,18 @@ class Constants {
   // ------- Url -------
   static const String baseURL = 'http://127.0.0.1:5000';
   static const String apiVersion = '$baseURL/api/v1';
+
+  // Auth Url
   static const String loginURL = '$apiVersion/login';
   static const String registerURL = '$apiVersion/register';
   static const String logoutURL = '$apiVersion/logout';
+
+  // User Url
   static const String userURL = '$apiVersion/user';
-  static const String postURL = '$apiVersion/posts';
+
+  // Post Url
+  static const String postURL = '$apiVersion/post/all';
+  static const String postdetailURL = '$apiVersion/post';
   static const String commentURL = '$apiVersion/comments';
 
   // ------ Status ------
