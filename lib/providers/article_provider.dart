cd9a33feec9cdd1f88ddd7228ee67f1cb437b019
Myo Thant <myothant.thedev@gmail.com>
2025-04-05 23:07:22 +0700

    fix(providers): fix article provider to work align with backend server

--- lib/providers/article_provider.dart
+++ lib/providers/article_provider.dart
@@ -9,18 +9,43 @@ class ArticleNotifier extends _$ArticleNotifier {
 
   final ArticleService _articleService = ArticleService();
 
+  int _currentPage = 1; // Tracking the pagination number
+  List<Article> _loadedArticles = []; // Store the articles that have been loaded
+
   @override
-  List<Article> build() {
-    _fetchArticles();
-    return [];
+  AsyncValue<List<Article>> build() {
+    // Initial loading state
+    return AsyncValue.loading();
   }
 
-  Future<void> _fetchArticles() async {
-    state = await _articleService.getAllArticle();
+  // Function to load articles
+  Future<void> loadArticles({int page = 1}) async {
+    state = AsyncValue.loading();
+    try {
+      final articles = await _articleService.getAllArticle(page);
+
+      // Append new articles to the existing list
+      _loadedArticles.addAll(articles);
+      _currentPage = page;
+      state = AsyncValue.data(_loadedArticles); // Update the state with new articles
+    } catch (e, stackTrace) {
+      state = AsyncValue.error(e, stackTrace); // If error, update with error state
+    }
   }
 
-  Future<Article> getDetailArticle(int id) async {
-    return await _articleService.getDetailArticle(id);
+  // Function to load a specific article by its ID
+  Future<AsyncValue<Article>> getDetailArticle(String id) async {
+    try {
+      final article = await _articleService.getDetailArticle(id);
+      return AsyncValue.data(article);
+    } catch (e, stackTrace) {
+      return AsyncValue.error(e, stackTrace);
+    }
   }
 
+  // Call this method to fetch the next page of articles (pagination)
+  Future<void> loadNextPage() async {
+    _currentPage++;
+    loadArticles(page: _currentPage);
+  }
 }
