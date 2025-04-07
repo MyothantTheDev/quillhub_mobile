6b73adb76cd1dff91b8de69048a0f00e63b08072
Myo Thant <myothant.thedev@gmail.com>
2025-04-07 22:57:23 +0700

    feat(models): add fromJson factory constructor for parsing from api response

--- lib/models/api_response.dart
+++ lib/models/api_response.dart
@@ -2,4 +2,15 @@ class ApiResponse {
   Object? data;
   String? message;
   int? status;
+
+  ApiResponse({this.data, this.message, this.status});
+
+  factory ApiResponse.fromJson(Map<String, dynamic> json) {
+
+    return ApiResponse(
+      data: json['data'],
+      message: json['message'],
+      status: json['status']
+    );
+  }
 }
