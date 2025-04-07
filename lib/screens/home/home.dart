25a269c6fb27b0c7761a06ce0b3f586ca41d5034
Myo Thant <myothant.thedev@gmail.com>
2025-04-07 23:04:28 +0700

    feat(screens): add infinite scroll feature at home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,13 +1,70 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quillhub/providers/article_provider.dart';
 import 'package:quillhub/providers/category_provider.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final category = ref.watch(categoryProvider);
-    return Text('This is $category Page.');
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+
+  final ScrollController _scrollController = ScrollController();
+  bool _isloading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
   }
+
+  void _scrollListener() {
+    double currentPosition = _scrollController.position.pixels;
+    double maxPosition = _scrollController.position.maxScrollExtent - 300;
+    if (currentPosition >= maxPosition && !_isloading) {
+      _loadMoreArticles();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _loadMoreArticles() async {
+    setState(() => _isloading = true);
+    await ref.read(articleNotifierProvider.notifier).loadNextPage();
+    setState(() => _isloading = false);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final category = ref.watch(categoryProvider);
+    final articles = ref.watch(articleNotifierProvider);
+
+    return ListView.builder(
+        controller: _scrollController,
+        itemCount: articles.length + (_isloading ? 1 : 0),
+        itemBuilder: (context, index) {
+
+          if (index == articles.length) {
+            return const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          final article = articles[index];
+          return ListTile(
+            title: Text('${index + 1} ${article.teaser?.title}' ?? 'No Title'),
+          );
+        },
+    );
+  }
+
 }
