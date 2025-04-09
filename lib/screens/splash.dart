6c778442ce1c74b1f272129ffd58a2c98d46ae5c
Myo Thant <myothant.thedev@gmail.com>
2025-04-10 00:06:18 +0700

    fix(screen): remove comments

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:quillhub/providers/article_provider.dart';
 
 class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Watch the article provider. Initial state is empty list.
-    // final articles = ref.watch(articleNotifierProvider);
-
-    // Listen to load articles.
-    // ref.listen(articleNotifierProvider, (prev, next) {
-    //   // Check the article list.
-    //   if(next.isNotEmpty) {
-    //     WidgetsBinding.instance.addPostFrameCallback((_) {
-    //       GoRouter.of(context).go('Home');
-    //     });
-    //   }
-    // });
 
     Future.delayed(Duration(seconds: 2))
     .then((value) => context.goNamed('home'));
